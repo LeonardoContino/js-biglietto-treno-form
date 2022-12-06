5f7d528df4643e060bc42066850b6840990e8f53
LeonardoContino <leoconti2000@hotmail.it>
2022-12-06 15:26:06 +0100

    elenco punti

--- main.js
+++ main.js
@@ -12,3 +12,14 @@ Sulla base di queste informazioni dovrà calcolare il prezzo totale del bigliett
 - va applicato uno sconto del 40% per gli over 65.
 
 */
+
+/*
+ 0. mettere elementi del dom
+ 1. chiedere il numero di chimoletri che deve fare con input
+ 2. chiedere l'età al passeggiero con input
+3.calcolare il prezzo del biglietto con un bottone
+4.applicare sconto minorenne e over 65 con il select
+5.validare km e età
+6.validare il prezzo in decimale
+7.fare l'estetica con il css
+*/
